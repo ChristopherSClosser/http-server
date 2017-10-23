85e24af01e386cfc05ec91888e8cee6cb17cf267
David Franklin <dave5801@gmail.com>
2017-10-23 16:52:17 -0700

    created server

--- server.py
+++ server.py
@@ -6,3 +6,26 @@ import sys
 server_address = ('localhost', 8080)
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('server is: ', server, '\nserver_address: ', server_address)
+server.bind(server_address)
+
+server.listen(1)
+
+while True:
+    print(sys.stderr, "waiting for a connection")
+    connection, client_address = server.accept()
+
+    try:
+        print(sys.stderr, "connection from", client_address)
+
+        while True:
+            data = connection.recv(16)
+            print(sys.stderr, "received %s" %data)
+            if data:
+                print(sys.stderr, "sending data back to the client")
+                connection.sendall(data)
+            else:
+                print(sys.stderr, "no more data from", client_address)
+                break
+
+    finally:
+        connection.close()
