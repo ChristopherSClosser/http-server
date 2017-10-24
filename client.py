5f3ce5a716a5ca763daa18a725566a667f3e1351
David Franklin <dave5801@gmail.com>
2017-10-23 17:27:34 -0700

    added text input arguments

--- client.py
+++ client.py
@@ -3,23 +3,28 @@
 import sys
 import socket
 
-server_address = ('localhost', 8080)
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print(sys.stderr, 'Connecting to port: %s' % server_address)
-server.connect(server_address)
+def main(message):
+    server_address = ('localhost', 8080)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #print(sys.stderr, 'Connecting to port: %s' % server_address)
+    server.connect(server_address)
 
 
-try:
-    message = 'A message'
-    server.sendall(message)
+    try:
+        server.sendall(message)
 
-    amount_rec = 0
-    amount_ex = len(message)
+        amount_rec = 0
+        amount_ex = len(message)
 
-    while amount_rec < amount_ex:
-        data = server.recv(16)
-        amount_rec += len(data)
-        print(sys.stderr, 'received %s ' % data)
+        while amount_rec < amount_ex:
+            data = server.recv(16)
+            amount_rec += len(data)
+            print(sys.stderr, 'received %s ' % data)
 
-finally:
-    server.close()
+    finally:
+        server.close()
+
+if __name__ == "__main__":
+    #pragma: no cover
+    #print(sys.argv)
+    main(sys.argv[1])
