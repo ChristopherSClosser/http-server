5e8433aca11c65b00570177da64b489e8193facc
David Franklin <dave5801@gmail.com>
2017-10-27 16:59:36 -0700

    test for length 1

--- src/server.py
+++ src/server.py
@@ -1,33 +1,38 @@
 """Simple echo server."""
+
+# -*- coding: utf-8 -*-
 import socket
 import sys
 
 
-server_address = ('localhost', 8080)
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-print('server_address: {}'.format(server_address))
-server.bind(server_address)
+def server_main():
+    server_address = ('localhost', 8080)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+    print('server_address: {}'.format(server_address))
+    server.bind(server_address)
 
-server.listen(1)
+    server.listen(1)
 
-while True:
-    print("waiting for a connection\n")
-    connection, client_address = server.accept()
+    while True:
+        print("waiting for a connection\n")
+        connection, client_address = server.accept()
 
-    try:
-        print("connection from", client_address)
+        try:
+            print("connection from", client_address)
 
-        while True:
-            data = connection.recv(16).decode('utf8')
-            print(sys.stderr, "received %s" % data)
-            if data:
-                print(sys.stderr, "sending data back to the client")
-                connection.sendall(data)
-            else:
-                print("no more data from", client_address)
-                break
+            while True:
+                data = connection.recv(16).decode('utf8')
+                print(sys.stderr, "received %s" % data)
+                if data:
+                    print(sys.stderr, "sending data back to the client")
+                    connection.sendall(data.encode())
+                else:
+                    print("no more data from", client_address)
+                    break
 
-    except KeyboardInterrupt:
-        connection.shutdown(socket.SHUT_WR)
-        connection.close()
-        sys.exit()
+        except KeyboardInterrupt:
+            connection.shutdown(socket.SHUT_WR)
+            connection.close()
+            sys.exit()
+if __name__ == '__main__':
+    server_main()
--- src/test_server.py
+++ src/test_server.py
@@ -8,10 +8,21 @@ def test_valid_client_string():
     message = "This is a test message"
     res = main(message)
     # print('this is what main(message) returns: ', res)
-    assert res == 22
+    assert res == "This is a test message"
 
 
 def test_case_fail():
     """test_case_fail."""
     with pytest.raises(Exception):
         main()
+
+def test_message_shorter_than_one_buffer():
+    message = "A"
+    res = main(message)
+    assert len(res) == 1
+
+#def test_message_several_buffers():
+
+#def test_multiple_msg_buffers_one_length():
+
+#def test_message_for_non_ascii():
